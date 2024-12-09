8a4b192e7aac1bd8eb3490be786573b5362b48cb
Ali-Kasal <muhamet.kasal@saintmichelannecy.fr>
2024-12-09 11:16:23 +0100

    Ajout de la fenetre windows

--- glouton/MainWindow.xaml.cs
+++ glouton/MainWindow.xaml.cs
@@ -1,37 +1,61 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 using Newtonsoft.Json;
 using OpenFoodFactsCSharp.Models;
 
 namespace OpenFoodFactsCSharp.Clients
 {
-    public class OpenFoodFactsApiLowLevelClient
+    public partial class MainWindow : Window
+
     {
-        private const string ApiUrl = "https://world.openfoodfacts.org/api/v0";
-        private readonly HttpClient _httpClient;
 
-        public OpenFoodFactsApiLowLevelClient(HttpClient httpClient)
+        public MainWindow()
+
         {
-            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+
+
+
         }
-
-        public async Task<ProductResponse> FetchProductByCodeAsync(string code)
+        public class OpenFoodFactsApiLowLevelClient
         {
-            var requestUri = $"{ApiUrl}/product/{code}.json";
-            var response = await _httpClient.GetAsync(requestUri);
+            private const string ApiUrl = "https://world.openfoodfacts.org/api/v0";
+            private readonly HttpClient _httpClient;
 
-            if (!response.IsSuccessStatusCode)
+            public OpenFoodFactsApiLowLevelClient(HttpClient httpClient)
             {
-                // Handle error or throw an exception
-                throw new HttpRequestException($"Failed to fetch product by code: {code}");
+                _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
             }
 
+            public async Task<ProductResponse> FetchProductByCodeAsync(string code)
+            {
+                var requestUri = $"{ApiUrl}/product/{code}.json";
+                var response = await _httpClient.GetAsync(requestUri);
 
-            var content = await response.Content.ReadAsStringAsync();
-            var product = JsonConvert.DeserializeObject<ProductResponse>(content);
+                if (!response.IsSuccessStatusCode)
+                {
+                    // Handle error or throw an exception
+                    throw new HttpRequestException($"Failed to fetch product by code: {code}");
+                }
 
-            return product;
+
+                var content = await response.Content.ReadAsStringAsync();
+                var product = JsonConvert.DeserializeObject<ProductResponse>(content);
+
+                return product;
+            }
         }
     }
 }
